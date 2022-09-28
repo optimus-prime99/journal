562695f762961f52bcbcfab32462a81615105909
optimus-prime99 <phankimhai123@gmail.com>
2022-09-28 17:23:37 +0700

    refactoring to Repository design pattern: 50%

--- app/Interfaces/PostRepositoryInterface.php
+++ app/Interfaces/PostRepositoryInterface.php
@@ -5,6 +5,18 @@ namespace App\Interfaces;
 interface PostRepositoryInterface
 {
 
+//    public function all();
+//
+//    public function store(array $data);
+//
+//    public function show(int $id);
+//
+//    public function edit(int $id);
+//
+//    public function update(array $data, int $id);
+//
+//    public function delete($id);
+
     public function all();
 
     public function store(array $data);
@@ -15,5 +27,5 @@ interface PostRepositoryInterface
 
     public function update(array $data, int $id);
 
-    public function delete($id);
+    public function delete(int $id);
 }
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -8,6 +8,78 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Session;
 class PostRepository implements PostRepositoryInterface
 {
+//    public function all()
+//    {
+////        return $posts = Post::all();
+//        return $posts = Post::latest()->paginate(5);
+//    }
+//
+//    public function store(array $data)
+//    {
+//        // TODO: Implement store() method.
+//
+////        return Post::create([
+////            'name' => $data['name'],
+////            'description' => $data['description'],
+////        ]);
+//        $post = Post::create([
+//            'name' => $data['name'],
+//            'description' => $data['description'],
+//        ]);
+//
+//        session()->flash('post-created-message', 'Post with name '.$data['name'].' was created');
+//
+//        return $post;
+//
+//    }
+//
+//    public function show(int $id)
+//    {
+//        // TODO: Implement show() method.
+//        $post = Post::find($id);
+//        if (!$post) {
+//            abort(404);
+//        }
+//        return $post;
+//    }
+//
+//    public function edit(int $id)
+//    {
+//        // TODO: Implement edit() method.
+//        $post = Post::find($id);
+//        if (!$post) {
+//            abort(404);
+//        }
+//        return $post;
+//    }
+//
+//    public function update(array $data, int $id)
+//    {
+//        // TODO: Implement update() method.
+//        $post = Post::find($id);
+//        if (!$post) {
+//            abort(404);
+//        }
+//        $post->update([
+//            'name' => $data['name'],
+//            'description' => $data['description'],
+//        ]);
+//        Session::flash('post-updated-message', 'Post with name '.$post['name'].' was updated');
+//        return $post;
+//    }
+//
+//    public function delete($id)
+//    {
+//        // TODO: Implement delete() method.
+//        $post = Post::find($id);
+//        if (!$post) {
+//            abort(404);
+//        }
+//        $post->delete();
+//        Session::flash('message', 'Post was deleted');
+//        return back();
+//    }
+
     public function all()
     {
 //        return $posts = Post::all();
@@ -22,15 +94,27 @@ class PostRepository implements PostRepositoryInterface
 //            'name' => $data['name'],
 //            'description' => $data['description'],
 //        ]);
+//        $inputs = request()->validate([
+//            'name' => 'required|min:8|max:255',
+//            'description' => 'required',
+//            'closed_at' => 'required'
+//        ]);
+
         $post = Post::create([
             'name' => $data['name'],
             'description' => $data['description'],
+            'closed_at' => $data['closed_at']
         ]);
-
-        session()->flash('post-created-message', 'Post with name '.$data['name'].' was created');
+//        $inputs = request()->validate($post);
+//        auth()->user()->posts()->create($inputs);
+//
+//        session()->flash('post-created-message', 'Post with name '.$data['name'].' was created');
 
         return $post;
 
+
+//        session()->flash('post-created-message', 'Post with name '.$inputs['name'].' was created');
+//        return $inputs;
     }
 
     public function show(int $id)
@@ -43,7 +127,7 @@ class PostRepository implements PostRepositoryInterface
         return $post;
     }
 
-    public function edit(int $id)
+        public function edit(int $id)
     {
         // TODO: Implement edit() method.
         $post = Post::find($id);
@@ -63,11 +147,11 @@ class PostRepository implements PostRepositoryInterface
         $post->update([
             'name' => $data['name'],
             'description' => $data['description'],
+            'closed_at' => $data['closed_at']
         ]);
         Session::flash('post-updated-message', 'Post with name '.$post['name'].' was updated');
         return $post;
     }
-
     public function delete($id)
     {
         // TODO: Implement delete() method.
@@ -79,4 +163,5 @@ class PostRepository implements PostRepositoryInterface
         Session::flash('message', 'Post was deleted');
         return back();
     }
+
 }
